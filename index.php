73b22a83b1f2f88a9b9afdaffd3d7a5de60d7d5a
KeyziaZulaikha <keyziazulaikhaa546@gmail.com>
2026-09-07 13:33:51 +0700

    Menambahkan tombol Main Lagi
    Added reset functionality to restart the game.

--- index.php
+++ index.php
@@ -11,6 +11,15 @@ if (!isset($_SESSION['angkaRahasia'])) {
 
 $angkaRahasia = $_SESSION['angkaRahasia'];
 
+if (isset($_POST['reset'])) {
+
+    $_SESSION['angkaRahasia'] = rand(1, 100);
+    $_SESSION['percobaan'] = 0;
+
+    header("Location: index.php");
+    exit;
+}
+
 if (isset($_POST['tebakan'])) {
 
     $tebakan = $_POST['tebakan'];
@@ -18,12 +27,26 @@ if (isset($_POST['tebakan'])) {
     $_SESSION['percobaan']++;
 
     if ($tebakan == $angkaRahasia) {
+
         echo "<p>🎉 Benar! Kamu berhasil menebak angka.</p>";
         echo "<p>Jumlah percobaan: " . $_SESSION['percobaan'] . "</p>";
+
+        echo '
+        <form method="post">
+            <button type="submit" name="reset">Main Lagi</button>
+        </form>
+        ';
+
+        exit;
+
     } elseif ($tebakan < $angkaRahasia) {
+
         echo "<p>⬆️ Terlalu kecil! Coba angka yang lebih besar.</p>";
+
     } else {
+
         echo "<p>⬇️ Terlalu besar! Coba angka yang lebih kecil.</p>";
+
     }
 }
 
