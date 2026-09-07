a9b55f9836fb12e0c759eae8ae81613d68342799
KeyziaZulaikha <keyziazulaikhaa546@gmail.com>
2026-09-07 13:02:51 +0700

    Membuat form tebakan
    Added a form for user input to guess the number.

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ echo "<h1>Game Tebak Angka</h1>";
 
 $angkaRahasia = rand(1, 100);
 
-echo "<p>Angka rahasia sudah dibuat.</p>";
-
 ?>
+
+<form method="post">
+    <label>Masukkan tebakan:</label>
+    <input type="number" name="tebakan" min="1" max="100" required>
+    <button type="submit">Tebak</button>
+</form>
